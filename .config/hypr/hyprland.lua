dcbf22014e617ffb151ea3570939cb47bba17c84
Bartodziej <bartixboss@gmail.com>
2026-06-04 12:05:10 +0200

    fix window resizing with keyboard

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -234,10 +234,34 @@ hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
-hl.bind(mainMod .. " + CTRL + left",  hl.dsp.window.resize({ x = -20, y =  0  }))
-hl.bind(mainMod .. " + CTRL + right", hl.dsp.window.resize({ x = 20,  y =  0  }))
-hl.bind(mainMod .. " + CTRL + up",    hl.dsp.window.resize({ x = 0,   y = -20 }))
-hl.bind(mainMod .. " + CTRL + down",  hl.dsp.window.resize({ x = 0,   y =  20 }))
+hl.bind(
+    mainMod .. " + CTRL + left",
+    hl.dsp.window.resize({ x = -20, y = 0,  relative = true }),
+    {
+        repeating = true
+    }
+)
+hl.bind(
+    mainMod .. " + CTRL + right",
+    hl.dsp.window.resize({ x = 20, y = 0,  relative = true }),
+    {
+        repeating = true
+    }
+)
+hl.bind(
+    mainMod .. " + CTRL + up",
+    hl.dsp.window.resize({ x = 0, y = -20,  relative = true }),
+    {
+        repeating = true
+    }
+)
+hl.bind(
+    mainMod .. " + CTRL + down",
+    hl.dsp.window.resize({ x = 0, y = 20,  relative = true }),
+    {
+        repeating = true
+    }
+)
 
 hl.bind("XF86AudioRaiseVolume", 
     hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
